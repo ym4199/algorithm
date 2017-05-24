99b240939c6d6f9f62b7dc3bb1a096a1ea453c17
ym4199 <ym4199@gmail.com>
2017-05-24 09:56:47 +0900

    bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,15 @@
+ori_list=[6,4,7,9,1,2,9,4,6]
+
+def bubble_sor(ori_list):
+    ori_len = len(ori_list)-1
+    for a in range(ori_len):
+        print('{}번째 loop1'.format(a))
+        for b in range(ori_len):
+            if ori_list[b] > ori_list[b+1]:
+                print('   {}번쨰 loop2'.format(b))
+                ori_list[b],ori_list[b+1] = ori_list[b+1],ori_list[b]
+                print('   {}와 {} 변화'.format(ori_list[b+1],ori_list[b]))
+                print(ori_list)
+        print(ori_list)
+
+bubble_sor(ori_list)
